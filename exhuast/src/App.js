042a58c15956cd940dec1e3f2eb7b9c3bcf01a14
mohammadawwad <mohd-awwad@hotmail.com>
2022-02-04 17:56:17 -0800

    setting up project structure

--- exhuast/src/App.js
+++ exhuast/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/Login';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import { useEffect, useState } from 'react';
+import axiosApp from './utils/axiosApp';
+import StatsPage from './components/StatsPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isAuthed, setIsAuthed] = useState('loading');
+
+	useEffect(() => {
+		(async function () {
+			const { data } = await axiosApp.get('/status');
+			console.log(data);
+			setIsAuthed(data.isAuthed);
+		})();
+	}, []);
+
+	return isAuthed === 'loading' ? (
+		<div className='total-center'>
+			<div class='spinner-border' role='status'>
+				<span class='visually-hidden'>Loading...</span>
+			</div>
+		</div>
+	) : !isAuthed ? (
+		<LoginPage />
+	) : (
+		<div>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path='/' exact component={HomePage} />
+					<Route path='/stats' exact component={StatsPage} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
